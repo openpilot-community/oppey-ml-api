fed907a6a24dbcde837a375c4ef49f8b8c375996
Joshua F. Rountree <joshua.rountree@scripps.com>
2019-02-08 13:04:37 -0500

    Adds static file serving in production

--- oppey_ml_api/settings.py
+++ oppey_ml_api/settings.py
@@ -56,8 +56,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'oppey_ml_api.urls'
 
 TEMPLATES = [
