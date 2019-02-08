21acbb6ae4f221d7e46f770e3712c1d134e6050a
Joshua F. Rountree <joshua.rountree@scripps.com>
2019-02-08 09:51:55 -0500

    Removed django_chatterbot models

--- oppey_ml_api/models/django_chatterbot_tag.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class DjangoChatterbotTag(models.Model):
-    name = models.CharField(unique=True, max_length=50)
-
-    class Meta:
-        managed = False
-        db_table = 'django_chatterbot_tag'
