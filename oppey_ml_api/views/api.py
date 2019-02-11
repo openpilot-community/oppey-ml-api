cf95d9931c188eb2a8920943e7b327474edcb1eb
Joshua F. Rountree <joshua.rountree@scripps.com>
2019-02-11 08:58:03 -0500

    Fixes to link conversations

--- oppey_ml_api/views/api.py
+++ oppey_ml_api/views/api.py
@@ -7,12 +7,11 @@ import re
 from django.db import transaction
 from chatterbot.ext.django_chatterbot import settings
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 from oppey_ml_api.models import DiscordMessages
 import logging
 logging.basicConfig(level=logging.INFO)
-oppey_chatbot = ChatBot(**settings.CHATTERBOT)
-
+chat_bot_instances = {}
 class ApiChatView(View):
     """
     Provide an API endpoint to interact with OppeyML.
@@ -25,15 +24,31 @@ class ApiChatView(View):
         * The JSON data should contain a 'text' attribute.
         """
         input_data = json.loads(request.body.decode('utf-8'))
-
         if 'text' not in input_data:
           return JsonResponse({
             'text': [
                 'The attribute "text" is required.'
             ]
           }, status=400)
-        
-        response = oppey_chatbot.get_response(input_data)
+        print(input_data);
+        if 'channel' not in input_data:
+          return JsonResponse({
+            'text': [
+                'The attribute "channel" is required.'
+            ]
+          }, status=400)
+        new_settings = {
+          'name': input_data.get('channel')
+        }
+        new_settings = {
+          **new_settings,
+          **settings.CHATTERBOT
+        }
+        if not (chat_bot_instances.get(input_data.get('channel'))):
+          chat_bot = ChatBot(**new_settings)
+        else:
+          chat_bot = chat_bot_instances.get(input_data.get('channel'))
+        response = chat_bot.get_response(input_data)
 
         response_data = response.serialize()
 
@@ -44,7 +59,7 @@ class ApiChatView(View):
         Return data corresponding to the current conversation.
         """
         return JsonResponse({
-            'name': oppey_chatbot.name
+            'name': chat_bot.name
         })
 
 class ApiTrainView(View):
@@ -53,32 +68,36 @@ class ApiTrainView(View):
     """
     
     def get(self, request, *args, **kwargs):
+      chat_bot = ChatBot(**settings.CHATTERBOT)
       print("Training Oppey from latest messages...")
-      messages_query = DiscordMessages.objects.select_for_update().filter(trained=False).order_by('discord_channel_id','created_at')
+      # messages_query = DiscordMessages.objects.select_for_update().filter(trained=False).order_by('discord_channel_id','created_at')
       print("Done querying data...")
       # messages = messages_query.values_list('content', flat=True)
       messages_to_train = []
-      
-      trainer = ListTrainer(oppey_chatbot)
-      with transaction.atomic():
-        print("Created ListTrainer...")
-        for message in messages_query:
-          print("Transforming message...{0}".format(message.id))
-          message.trained = True
-          message_content = message.content
-          if len(message_content):
-            message_content = re.sub(r'\<\@\![0-9]+\>', '', message_content)
-            message_content = re.sub(r'\<\@\[0-9]+\>', '', message_content)
+      convo_trainer = ChatterBotCorpusTrainer(chat_bot)
+      # trainer = ListTrainer(chat_bot)
+      convo_trainer.train(
+        "chatterbot.corpus.english.greetings",
+        "chatterbot.corpus.english.conversations")
+      # with transaction.atomic():
+      #   print("Created ListTrainer...")
+      #   for message in messages_query:
+      #     print("Transforming message...{0}".format(message.id))
+      #     message.trained = True
+      #     message_content = message.content
+      #     if len(message_content):
+      #       message_content = re.sub(r'\<\@\![0-9]+\>', '', message_content)
+      #       message_content = re.sub(r'\<\@[0-9]+\>', '', message_content)
           
-          if message.attachment_ids.get('0'):
-            message_content = message_content + " " + message.attachment_ids.get('0').get('url')
-          message_content = ' '.join(message_content.split())
-          message_content = message_content.replace(' ,',',')
+      #     if message.attachment_ids.get('0'):
+      #       message_content = message_content + " " + message.attachment_ids.get('0').get('url')
+      #     message_content = ' '.join(message_content.split())
+      #     message_content = message_content.replace(' ,',',')
 
-          print("Appending message to array...{0}".format(message.id))
-          messages_to_train.append(message_content)
-      print("Now training ... {0}".format(len(messages_to_train)))
-      trainer.train(messages_to_train)
+      #     print("Appending message to array...{0}".format(message.id))
+      #     messages_to_train.append(message_content)
+      # print("Now training ... {0}".format(len(messages_to_train)))
+      # trainer.train(messages_to_train)
       messages_query.update(trained=True)
       return JsonResponse({
         'message': 'Oppey has been successfully trained.',
